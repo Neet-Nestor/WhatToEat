faebb03b31fcc449316f0a571dc6b55de1956080
Neet <imba.qxy@gmail.com>
2017-11-06 11:00:20 -0800

    Basic UI content

---
+++ WhatToEat/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by Student User on 11/6/17.
+//  Copyright © 2017 Nestor Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var startBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set UI styles
+        startBtn.layer.borderWidth = 2
+        startBtn.layer.cornerRadius = startBtn.frame.width / 30
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

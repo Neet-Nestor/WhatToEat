db4741afac6ea29dd1eeced782d9a13476a01820
NeetNestor <imba.qxy@gmail.com>
2017-11-23 02:23:28 -0800

    Ver 0.15
    Add details to StoryBoard

---
+++ WhatToEat/RatingControl.swift
@@ -0,0 +1,28 @@
+//
+//  RatingControl.swift
+//  WhatToEat
+//
+//  Created by Student User on 11/22/17.
+//  Copyright © 2017 Nestor Qin. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
---
+++ WhatToEat/RestaurantListCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantListCell.swift
+//  WhatToEat
+//
+//  Created by Student User on 11/23/17.
+//  Copyright © 2017 Nestor Qin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

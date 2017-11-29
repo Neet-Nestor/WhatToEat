175416e0c2309881a6596c9d6ad44e7d29b53e6a
NeetNestor <imba.qxy@gmail.com>
2017-11-28 19:10:32 -0800

    add some windows

--- WhatToEat/FoundViewController.swift
+++ WhatToEat/FoundViewController.swift
@@ -1,29 +1,31 @@
 //
-//  PreviewListViewController.swift
+//  FoundViewController.swift
 //  WhatToEat
 //
-//  Created by Student User on 11/27/17.
+//  Created by Student User on 11/28/17.
 //  Copyright © 2017 Nestor Qin. All rights reserved.
 //
 
 import UIKit
 
-class PreviewListViewController: UIViewController {
+class FoundViewController: UIViewController {
 
-    @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var theNavigationItem: UINavigationItem!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationItem.title = "Congratulation!"
+    }
+
     /*
     // MARK: - Navigation
 

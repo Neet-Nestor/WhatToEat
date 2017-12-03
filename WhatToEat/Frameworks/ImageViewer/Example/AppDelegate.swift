2c5eb7933992ec21ba88f838519eb08b6e9365ec
NeetNestor <imba.qxy@gmail.com>
2017-12-03 01:07:21 -0800

    include necessary files

---
+++ WhatToEat/Frameworks/ImageViewer/Example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Rui Peres on 05/12/2015.
+//  Copyright © 2015 MailOnline. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}

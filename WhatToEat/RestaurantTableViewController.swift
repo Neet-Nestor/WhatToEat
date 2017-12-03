cfea472d1868708ab5e15bee190bffcdcbb40204
Jeff <zhengzichu@gmail.com>
2017-12-03 01:53:21 -0800

    Start working on list of restaurant

---
+++ WhatToEat/RestaurantTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantTableViewController.swift
+//  WhatToEat
+//
+//  Created by zichu zheng on 12/3/17.
+//  Copyright © 2017 Nestor Qin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

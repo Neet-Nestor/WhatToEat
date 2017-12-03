57d62eeef20dc81283f9d073feefc2a937c2a808
Jeff <zhengzichu@gmail.com>
2017-12-03 01:53:48 -0800

    add RestaurantTableViewController.swift

--- WhatToEat/RestaurantTableViewController.swift
+++ WhatToEat/RestaurantTableViewController.swift
@@ -8,17 +8,53 @@
 
 import UIKit
 
-class RestaurantTableViewController: UIViewController {
+class RestaurantTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let latitude = 47.656893
+        let longitude = -122.313282
+        
+        let url = "https://api.yelp.com/v3/businesses/search?term=restuarant&latitude=\(latitude)&longitude=\(longitude)"
+        let tokenString = "Bearer XC28UCfVfPvioZMT3WdKcZSuf9KgrHeJtWcEyog3xNOkgGJCSFY_Lax7GC6KGwOA2qbFaOS-h9KlYWNP8ihIkrInjz-TcDKGHzrKGLXC89JZkmSkxbolzD6wqcIUWnYx"
+        var request = URLRequest(url: URL(string: url)!)
+        request.addValue(tokenString, forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession.shared
+        
+        let tache = session.dataTask(with: request) { (data, response, error) -> Void in
+            let responseParse = response as! HTTPURLResponse
+            if (responseParse.statusCode == 200) {
+                //
+                let dict = try! JSONSerialization.jsonObject(with: data!, options: [])
 
-        // Do any additional setup after loading the view.
+                
+                
+            } else {
+                let alert = UIAlertController(title: "Alert", message: "statusCode: \(responseParse.statusCode)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        tache.resume()
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RestaurantListCell
+        //        cell.myImage.image = UIImage(named: ("\(indexPath.row % 3 + 1).jpg"))
+        return cell
     }
     
 

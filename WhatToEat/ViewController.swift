9efe94b565524d9eaa87881ee6e80493b78b4046
Jeff <zhengzichu@gmail.com>
2017-12-05 18:11:01 -0800

    add launchScreen

--- WhatToEat/ViewController.swift
+++ WhatToEat/ViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     // Outlets
-    @IBOutlet weak var startBtn: UIButton!
+    //@IBOutlet weak var startBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // set UI styles
-        startBtn.layer.borderWidth = 2
-        startBtn.layer.cornerRadius = startBtn.frame.width / 30
+//        startBtn.layer.borderWidth = 2
+//        startBtn.layer.cornerRadius = startBtn.frame.width / 30
     }
 
     override func didReceiveMemoryWarning() {
